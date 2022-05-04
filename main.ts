705c5996226dfec295f7dbfe4cbc92c66309fdca
amartinhua2 <93262314+amartinhua2@users.noreply.github.com>
2022-05-04 14:01:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,22 @@
-input.onButtonPressed(Button.A, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     while (true) {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.PINK)
+        basic.pause(pausa)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.OFF)
-        basic.pause(1000)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.CYAN)
+        basic.pause(pausa)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.OFF)
-        basic.pause(1000)
     }
 })
+input.onButtonPressed(Button.A, function () {
+    pausa += 50
+})
+input.onButtonPressed(Button.B, function () {
+    pausa += -50
+})
+let pausa = 0
 DFRobotMaqueenPlus.I2CInit()
+pausa = 1000
+basic.forever(function () {
+    basic.showNumber(pausa)
+})
